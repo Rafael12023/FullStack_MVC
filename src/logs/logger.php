5f7e190e246021bb57f38ef2b1e774af15f9a276
Larissa Melo <152574409+larissml@users.noreply.github.com>
2025-04-22 19:44:56 -0300

    adicionando desc nos logs logger.php
    adicionando IP nos logs

--- src/logs/logger.php
+++ src/logs/logger.php
@@ -2,10 +2,11 @@
 date_default_timezone_set('America/Sao_Paulo');
 function registrarLogTxt($tipo, $mensagem, $username = null) {
     $arquivo = __DIR__ . '/../logs/app.txt';
-    $data = date('Y-m-d H:i:s'); // Data atual
+    $data = date('Y-m-d H:i:s'); 
     $usuarioInfo = $username ? "Usuário: $username" : "Usuário: Não identificado";
-
-    $log = "[$data] [$tipo] $mensagem - $usuarioInfo\n";
+    $hostname = gethostname();
+    $ip = gethostbyname($hostname);
+    $log = "[$data] [$tipo] $mensagem - $usuarioInfo | IP: $ip\n";
 
     file_put_contents($arquivo, $log, FILE_APPEND);
 }
