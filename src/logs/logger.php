387330657c9bcc7669b74cc4ab7c7e716cd7d58d
Jenifer Kelly <jeniferkelly2017@gmail.com>
2025-04-22 21:45:29 -0300

    Tratativa de exceção na função gerar log

--- src/logs/logger.php
+++ src/logs/logger.php
@@ -1,7 +1,8 @@
 <?php
 date_default_timezone_set('America/Sao_Paulo');
 function registrarLogTxt($tipo, $mensagem, $username = null) {
-    $arquivo = __DIR__ . '/../logs/app.txt';
+    try {
+        $arquivo = __DIR__ . '/../logs/app.txt';
     $data = date('Y-m-d H:i:s'); 
     $usuarioInfo = $username ? "Usuário: $username" : "Usuário: Não identificado";
     $hostname = gethostname();
@@ -9,4 +10,7 @@ function registrarLogTxt($tipo, $mensagem, $username = null) {
     $log = "[$data] [$tipo] $mensagem - $usuarioInfo | IP: $ip\n";
 
     file_put_contents($arquivo, $log, FILE_APPEND);
+    } catch (Exception $e) {
+        error_log("Erro ao registrar log!". $e->getMessage());
+    }
 }
