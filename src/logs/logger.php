fce4ecc5d06f19f819654d42cd840d076b517771
caua-lucas <caual5397@gmail.com>
2025-04-27 11:49:03 -0300

    Implementação de condicionais no arquivo logger.php

--- src/logs/logger.php
+++ src/logs/logger.php
@@ -3,6 +3,14 @@ date_default_timezone_set('America/Sao_Paulo');
 function registrarLogTxt($tipo, $mensagem, $username = null) {
     try {
         $arquivo = __DIR__ . '/../logs/app.txt';
+        if (!is_file($arquivo)) {// Verifica se o arquivo não existe
+            file_put_contents($arquivo, "");//Caso não seja um arquivo, cria o arquivo com o conteudo vazio.
+        }
+        if (!is_writable($arquivo)) { // Verifica se o arquivo não tem permissão de escrita
+            error_log("Arquivo de log encontrado, mas sem permissão de escrita: " . $arquivo);
+            return;
+        }
+
     $data = date('Y-m-d H:i:s'); 
     $usuarioInfo = $username ? "Usuário: $username" : "Usuário: Não identificado";
     $hostname = gethostname();
