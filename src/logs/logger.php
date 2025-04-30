3bb444b38d9e5b084c7680a7d11d3c167e5d6faf
caua-lucas <caual5397@gmail.com>
2025-04-30 15:08:55 -0300

    Melhoria no tratamento de erros, com validação de mensagem, criação de arquivo e checagem de falhas na gravação.

--- src/logs/logger.php
+++ src/logs/logger.php
@@ -2,7 +2,13 @@
 date_default_timezone_set('America/Sao_Paulo');
 function registrarLogTxt($tipo, $mensagem, $username = null) {
     try {
+        if (empty($mensagem)) {
+            error_log("Mensagem de log vazia."); //Evita logs inúteis ou registros acidentais.
+            return;
+        }
+
         $arquivo = __DIR__ . '/../logs/app.txt';
+
         if (!is_file($arquivo)) {// Verifica se o arquivo não existe
             file_put_contents($arquivo, "");//Caso não seja um arquivo, cria o arquivo com o conteudo vazio.
         }
@@ -17,7 +23,12 @@ function registrarLogTxt($tipo, $mensagem, $username = null) {
     $ip = gethostbyname($hostname);
     $log = "[$data] [$tipo] $mensagem - $usuarioInfo | IP: $ip\n";
 
-    file_put_contents($arquivo, $log, FILE_APPEND);
+    $resultado_gravacao = file_put_contents($arquivo, $log, FILE_APPEND);
+
+    if ($resultado_gravacao === false) {
+        error_log("Falha ao escrever no arquivo de log: $arquivo");
+    }
+    
     } catch (Exception $e) {
         error_log("Erro ao registrar log!". $e->getMessage());
     }
