0c752481df9d14790c49489def4b310888e53957
Larissa Melo <152574409+larissml@users.noreply.github.com>
2025-05-26 21:18:08 -0300

    Update logger.php
    retirada da função de logTXT e adicionando a funçao de inserir log no BD.

--- src/logs/logger.php
+++ src/logs/logger.php
@@ -1,32 +1,33 @@
 <?php
 date_default_timezone_set('America/Sao_Paulo');
-function registrarLogTxt($tipo, $mensagem, $username = null) {
+function registrarLogBanco($tipo, $mensagem, $username = null) {
+
+    global $pdo;
     try {
         if (empty($mensagem)) {
             error_log("Mensagem de log vazia."); //Evita logs inúteis ou registros acidentais.
             return;
         }
 
-        $arquivo = __DIR__ . '/../logs/app.txt';
-
-        if (!is_file($arquivo)) {// Verifica se o arquivo não existe
-            file_put_contents($arquivo, "");//Caso não seja um arquivo, cria o arquivo com o conteudo vazio.
-        }
-        if (!is_writable($arquivo)) { // Verifica se o arquivo não tem permissão de escrita
-            error_log("Arquivo de log encontrado, mas sem permissão de escrita: " . $arquivo);
-            return;
-        }
 
     $data = date('Y-m-d H:i:s'); 
     $usuarioInfo = $username ? "Usuário: $username" : "Usuário: Não identificado";
     $hostname = gethostname();
     $ip = gethostbyname($hostname);
-    $log = "[$data] [$tipo] $mensagem - $usuarioInfo | IP: $ip\n";
 
-    $resultado_gravacao = file_put_contents($arquivo, $log, FILE_APPEND);
+    $stmt = $pdo->prepare("INSERT INTO logs (data_hora, nivel, mensagem, usuario, ip) VALUES (:data_hora, :nivel, :mensagem, :usuario, :ip)");
+    $stmt->execute([
+        ':data_hora' => $data,
+        ':nivel'     => $tipo,
+        ':mensagem'  => $mensagem,
+        ':usuario'   => $usuarioInfo,
+        ':ip'        => $ip
+    ]);
 
-    if ($resultado_gravacao === false) {
-        error_log("Falha ao escrever no arquivo de log: $arquivo");
+    $dados = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (!$dados) {
+        error_log("Falha ao gerar log");
     }
     
     } catch (Exception $e) {
